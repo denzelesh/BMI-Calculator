6caed73f5365fd2f18811750b1603e1f726f52a2
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-03-05 19:25:17 +0000

    Added Constants, Menu & Data Validation
    ... Using parse to ensure data added is accurate

--- program.cs
+++ program.cs
@@ -1,5 +1,12 @@
 class BMIapp
 {
+    // WHO <- BMI Ratings
+    const double Underweight = 18.50;
+    const double Normal = 24.90;
+    const double Overweight = 29.90;
+    const double ObeseClass_1 = 34.90;
+    const double ObeseClass_2 = 39.90;
+    const double ObeseClass_3 = 40.00;
 
 
     static void AuthorName()
@@ -22,11 +29,31 @@ class BMIapp
         Console.WriteLine("-----------------------------------");
         Console.WriteLine(" ");
     }
+    
+    static void CalculateBMI()
+    {
+        Console.WriteLine("Choose your preferred units of measurement:");
+        Console.WriteLine("1. Imperial Units");
+        Console.WriteLine("2. Metric Units");
+        
+        int unitChoice; // Variable Declaration
+
+         // Adds a parse to check and make sure correct data type has been entred
+        if (!int.TryParse(Console.ReadLine(), out unitChoice))
+        {
+            //Displays error message if an incorrect conversion...
+            //...option is chosen
+            Console.WriteLine("Incorrect input");
+            return;
+        }
+
+    }
 
 
     static void Main()  
     {
         AuthorName();
+        CalculateBMI();
 
     }
 }
