fb17a10a23d5f351529c0ca9be7cf0d28798591a
Denzel <114736457+denzelesh@users.noreply.github.com>
2023-03-05 19:06:46 +0000

    Created program.cs file and added app explanation

---
+++ program.cs
@@ -0,0 +1,22 @@
+class BMIapp
+{
+
+
+    static void AuthorName()
+    {
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("   App 02 - BMI Calculator");
+        Console.WriteLine("         By Denzel Eshun");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("   This application will calculate");
+        Console.WriteLine("     your basal metabolic rate");
+        Console.WriteLine("-----------------------------------");
+    }
+
+
+    static void Main()  
+    {
+        AuthorName();
+
+    }
+}
